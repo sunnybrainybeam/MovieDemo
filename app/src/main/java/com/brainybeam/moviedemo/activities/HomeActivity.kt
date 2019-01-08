efc199a7a30fa99b42d8a7ee00fa46ae04d0431b
Sunny Desai <sunny@brainybeam.com>
2019-01-08 21:45:57 +0530

    Add auto scroll every 5 seconds.

--- app/src/main/java/com/brainybeam/moviedemo/activities/HomeActivity.kt
+++ app/src/main/java/com/brainybeam/moviedemo/activities/HomeActivity.kt
@@ -1,11 +1,14 @@
 package com.brainybeam.moviedemo.activities
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.viewpager.widget.ViewPager
 import com.brainybeam.moviedemo.R
 import com.brainybeam.moviedemo.adapters.HomePagerAdapter
 import com.brainybeam.moviedemo.baseStructures.BaseAppCompactActivity
@@ -21,6 +24,11 @@ class HomeActivity : BaseAppCompactActivity() {
     private lateinit var binding: ActivityHomeBinding
     private lateinit var mMovieViewModel: MovieViewModel
     private lateinit var homePagerAdapter: HomePagerAdapter
+    private val handler = Handler()
+    private val autoSwipeTime: Long = 5000 //5 Second
+    private var pageNo = 0
+    private var isSwiping = false
+    private lateinit var runnable: Runnable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +36,55 @@ class HomeActivity : BaseAppCompactActivity() {
         binding = getViewBinding()
         setToolbar(binding.toolbarHome, getString(R.string.movie), false)
         mMovieViewModel = ViewModelProviders.of(this).get(MovieViewModel::class.java)
+        setRunnable()
+        setPagerListener()
         callHomeMovieListApi()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startSwiping()
+    }
+
+    override fun onPause() {
+        stopSwiping()
+        super.onPause()
+    }
+
+    private fun setRunnable() {
+        runnable = object : Runnable {
+            override fun run() {
+                if (::homePagerAdapter.isInitialized) {
+                    if (homePagerAdapter.count == pageNo) {
+                        pageNo = 0
+                    } else {
+                        pageNo++
+                    }
+                    binding.viewPagerMovie.setCurrentItem(pageNo, true)
+                }
+                handler.postDelayed(this, autoSwipeTime)
+            }
+        }
+    }
+
+    private fun setPagerListener() {
+        binding.viewPagerMovie.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                pageNo = position
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                if (state == ViewPager.SCROLL_STATE_DRAGGING) {
+                    stopSwiping()
+                } else {
+                    startSwiping()
+                }
+            }
+        })
     }
 
     private fun callHomeMovieListApi() {
@@ -44,6 +100,7 @@ class HomeActivity : BaseAppCompactActivity() {
                             setPadding(200, 50, 200, 50)
                             pageMargin = 100
                             setPageTransformer(false, HomePagerTransformation(this@HomeActivity))
+                            startSwiping()
                         }
                     } else {
                         Utility.showToastMessage(this@HomeActivity, it.message)
@@ -66,5 +123,17 @@ class HomeActivity : BaseAppCompactActivity() {
             Utility.showToastMessage(this, getString(R.string.search))
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun startSwiping() {
+        if (!isSwiping) {
+            isSwiping = true
+            handler.postDelayed(runnable, autoSwipeTime)
+        }
+    }
+
+    private fun stopSwiping() {
+        isSwiping = false
+        handler.removeCallbacks(runnable)
     }
 }
